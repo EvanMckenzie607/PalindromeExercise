a228b02601b06c8f5943fe1704223f5fbc246b1d
Evan Mckenzie <615evan@gmail.com>
2022-08-31 14:26:11 -0400

    done

---
+++ PalindromeExercise/PalindromeExercise/WordSmith.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PalindromeExercise
+{
+    public class WordSmith
+    {
+        public bool IsAPalindrome(string input)
+        {
+
+            var reversed = "";
+
+            for (int i = input.Length -1; i >= 0; i--)
+            {
+                reversed += input[i];
+            }
+            if (reversed == input)
+            {
+                return true;
+
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+
+
+
+
+    }
+}
--- PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
+++ PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
@@ -1,14 +1,23 @@
+using PalindromeExercise;
 using System;
 using Xunit;
 
 namespace PalindromeExerciseTests
 {
-    public class UnitTest1
+    public class WordTest
     {
-        [Fact]
-        public void Test1()
+        [Theory]
+        [InlineData("racecar", true)]
+        [InlineData("hello", false)]
+        [InlineData("helping", false)]
+        [InlineData("tacocat", true)]
+        public void Test1(string input, bool expected)
         {
+            var x = new WordSmith();
+            
+            var outcome =  x.IsAPalindrome(input);
 
+            Assert.Equal(expected, outcome);
         }
     }
 }
